6c5a9665b91e3b973f3dca57811c6ffef8a4b4ab
Evan Bergeron <bergeronej@gmail.com>
2017-05-01 04:52:06 -0400

    Implementing ordering by cost per calorie

--- main.py
+++ main.py
@@ -1,14 +1,40 @@
+import csv
+import sys
+from pprint import pprint
 from collections import namedtuple
-def to_namedtuples(filename):
+class Grocery(object):
+    def __init__(self, name, price, store, calories):
+        self.name = name
+        self.price = float(price)
+        self.store = store
+        self.calories = int(calories)
+
+def parse_groceries(filename):
     result = []
     with open(filename, mode='rb') as infile:
-        # wtf python namespaces
-        print csv
         reader = csv.reader(infile)
-        Data = namedtuple("Data", next(reader))
-        result.append(Data)
+        for row in reader:
+            try:
+                (name, price, store, calories) = tuple(row)
+            except ValueError:
+                print row
+                sys.exit(1)
+            try:
+                item = Grocery(name, price, store, calories)
+            except ValueError:
+                # Ignore things with TODO in csv right now
+                continue
+            result.append(item)
     return result
 
+def order_by_cost_per_calories(groceries):
+    return [name for _, name in sorted([ \
+        (grocery.price / grocery.calories, grocery.name) \
+        for grocery in groceries]
+        )]
+
 if __name__ == '__main__':
-    csv = 'groceries.csv'
-    print to_namedtuples(csv)
+    csv_file = 'groceries.csv'
+    groceries = parse_groceries(csv_file)
+    for item in order_by_cost_per_calories(groceries):
+        print item
